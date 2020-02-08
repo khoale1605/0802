2545fe2f94ec0a61aa61b1126c13b9f5f119376f
khoale1605 <57674103+khoale1605@users.noreply.github.com>
2020-02-08 13:04:05 +0700

    Update pipeline-job.groovy

--- pipeline-job.groovy
+++ pipeline-job.groovy
@@ -27,11 +27,12 @@ pipeline {
         }
         stage('Msg Notification') {
             steps {                                
-                mail bcc: '', body: '''Dear all,
+                mail bcc: '', body: '''
+                Dear all,
 
-                    This mail is sending with purpose is testing build pipeline job from our Jenkins Server.
+                This mail is sending with purpose is testing build pipeline job from our Jenkins Server.
 
-                    Best Regards.''', cc: 'ledangkhoa95@outlook.com.vn', from: '', replyTo: '', subject: 'Test build', to: 'dangkhoa22031995@gmail.com'               
+                Best Regards.''', cc: 'ledangkhoa95@outlook.com.vn', from: '', replyTo: '', subject: 'Test build', to: 'dangkhoa22031995@gmail.com'               
             }
         }
     }
